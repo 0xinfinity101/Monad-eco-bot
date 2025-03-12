255d42dfcfb61fcb2532469d52aca980138ae207
0xinfinity <38694992+0xinfinity101@users.noreply.github.com>
2025-03-12 13:49:29 +0800

    Update displayHeader.js

--- src/displayHeader.js
+++ src/displayHeader.js
@@ -3,7 +3,7 @@ require("colors");
 function displayHeader() {
   process.stdout.write("\x1Bc");
   console.log("========================================".magenta);
-  console.log("=   Monad Testnet Bot  by 101parallax  =".magenta);
+  console.log("=  Monad Testnet Bot by 0xinfinity101  =".magenta);
   console.log("========================================".magenta);
   console.log("");
 }
